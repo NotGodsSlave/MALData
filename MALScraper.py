f190128347c2c6a3262c5fb40a5827923c862804
NotGodsSlave <notgodsslave@gmail.com>
2022-03-09 18:58:02 +0100

    initial commit

---
+++ MALScraper.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import time
+import sys, argparse
+
+class MALScraper:
+    def __init__(self, proxies = None):
+        self.titles = []
+        self.mal_genres = ["Action", "Adventure", "Boys Love", "Comedy", "Drama", "Fantasy", "Girls Love", "Horror", "Mystery", "Romance", "Sci-Fi", "Slice of Life",
+              "Sports", "Supernatural", "Ecchi", "Hentai", "Cars", "Demons", "Game", "Harem", "Historical", "Martial Arts", "Mecha", "Military", "Music",
+              "Parody", "Police", "Psychological", "Samurai", "School", "Space", "Supew Power", "Vampire"]
+        self.proxies = proxies
+        self.proxy_counter = 0
+
+    def get_id_from_link(self, url):
+        return int(url.split('/')[4])
+
+    def get_mal_anime_link(self, id):
+        return f"https://myanimelist.net/anime/{id}"
+
+    def scrape_anime(self, start = 0, lim = 1000):
+        counter = start
+        while counter < lim:
+            url = f"https://myanimelist.net/topanime.php?limit={counter}"
+            req = requests.get(url)
+            if req.status_code != 200:
+                print(f"Error scraping top anime page for limit {counter}, status code {req.status_code}")
+            else:
+                soup = BeautifulSoup(req.content, "html.parser")
+                tds = soup.select("td.title.al.va-t.word-break")
+                for td in tds:
+                    self.titles.append(self.get_title(td.find('a')['href']))
+                    counter = counter + 1
+                    if counter % 100 == 0:
+                        print(f'{counter-start} titles scraped')
+                        time.sleep(180) # use if scraping without proxy to avoid IP ban
+                    if counter >= lim:
+                        break
+    
+    def get_title(self, url):
+        id = self.get_id_from_link(url)
+        if self.proxies == None:
+            req = requests.get(url)
+        else:
+            print(self.proxy_counter)
+            req = requests.get(url, proxies = self.proxies[self.proxy_counter])
+            self.proxy_counter = (self.proxy_counter + 1) % self.proxies.size()
+        title_dict = {"id": id, "Title": None, "Score": None, "Scored by": None, "Members": None,
+                 "Genres": None, "Status": None}
+        if req.status_code != 200:
+            print(f"Error scraping the page with id {id}, status code {req.status_code}")
+        else:
+            soup = BeautifulSoup(req.content, "html.parser")
+            title_dict["Title"] = soup.find(class_="title-name").text
+            title_dict["Score"] = float(soup.find("span", class_="score-label").text)
+            title_dict["Scored by"] = int(soup.find("span", itemprop="ratingCount").text)
+            title_dict["Members"] = int(soup.find("span",string="Members:").parent.text.replace("Members:","").replace(",","").strip())
+            title_dict["Genres"] = [g.text for g in soup.find_all("span",itemprop="genre")]
+            title_dict["Status"] = soup.find("span", string="Status:").parent.text.replace("Status:","").strip()
+            for genre in self.mal_genres:
+                title_dict[genre] = int(genre in title_dict["Genres"])
+
+        return title_dict
+
+    def titles_to_csv(self):
+        df = pd.DataFrame(data=self.titles)
+        df.to_csv("titles.csv", index=False)
+
+    def load_from_csv(self):
+        df = pd.read_csv("titles.csv")
+        self.titles = df.to_dict("records")
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser("Scraper parser")
+    arg_parser.add_argument('--start', type=int, required=False, default=0,
+                            help="Number of first title scrapped, sorted by score")
+    arg_parser.add_argument('--limit', type=int, required=False, default=1000,
+                            help="Number of title at which scrapping stops, sorted by score")
+    args = arg_parser.parse_args()
+    mals = MALScraper()
+    mals.scrape_anime(args.start, args.limit)
+    mals.titles_to_csv()
---
+++ MALVisual.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+mal_genres = ["Action", "Adventure", "Boys Love", "Comedy", "Drama", "Fantasy", "Girls Love", "Horror", "Mystery", "Romance", "Sci-Fi", "Slice of Life",
+              "Sports", "Supernatural", "Ecchi", "Hentai", "Cars", "Demons", "Game", "Harem", "Historical", "Martial Arts", "Mecha", "Military", "Music",
+              "Parody", "Police", "Psychological", "Samurai", "School", "Space", "Supew Power", "Vampire"]
+
+def genre_correlation(df):
+    df_genres = df[mal_genres]
+    for genre in mal_genres:
+        if not (True in set(df_genres[genre])):
+            df_genres = df_genres.drop(genre, axis=1)
+    corr = df_genres.corr()
+    f, ax = plt.subplots(figsize=(16, 12))
+    cmap = sns.diverging_palette(250, 10, as_cmap=True)
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    sns.heatmap(corr, cmap=cmap, mask=mask)
+    plt.show()
+
+def genre_scores(df):
+    genre_scores = {}
+    for genre in mal_genres:
+        if 1 in df[genre].values:
+            genre_scores[genre] = df.groupby(genre)['Score'].mean()[1]
+    plt.figure(figsize = (24,12))
+    plt.bar(list(genre_scores.keys()), genre_scores.values(), color='b')
+    plt.show()
+
+def score_distribution(df, bins):
+    scores = df['Score']
+    plt.hist(scores, bins = bins)
+    plt.show()
+
+if __name__ == "__main__":
+    df = pd.read_csv('titles.csv')
+    genre_correlation(df)
+    genre_scores(df)
+    score_distribution(df, 20)
+
